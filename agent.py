e088807ed1c3065bad20afee6b1b188c1c83a9f8
prasanna <prasannamukherjee06@gmail.com>
2026-04-18 02:35:45 +0530

    changed the llm

--- agent.py
+++ agent.py
@@ -8,13 +8,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation",
-    pipeline_kwargs={"temperature": 0.7, "max_new_tokens": 100}
-)
 
-model = ChatHuggingFace(llm = llm)
+
+model = ChatGoogleGenerativeAI(model= "gemini-3-flash-preview")
 parser = StrOutputParser()
 
 
