5e65b123f82b551dc5dba202952d71f2432716d8
prasanna <prasannamukherjee06@gmail.com>
2026-04-22 11:04:15 +0530

    changed the model name

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 
 
-model = ChatGoogleGenerativeAI(model= "Gemini 3.1 Flash-Lite Preview")
+model = ChatGoogleGenerativeAI(model= "gemini-3-flash-preview")
 parser = StrOutputParser()
 
 
